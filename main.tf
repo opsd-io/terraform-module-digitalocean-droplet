cdd9c62ce647f09a3b18cae9e6f87de21b3a2d21
michal.browarczyk@zdt.io <michal.browarczyk@zdt.io>
2024-03-12 11:20:32 +0100

    basic changes

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
-# Terraform code goes here
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "web" {
+  image  = "ubuntu-20-04-x64"
+  name   = "web-1"
+  region = "nyc2"
+  size   = "s-1vcpu-1gb"
+}# Terraform code goes here
