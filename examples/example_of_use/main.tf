3b01617c0f499cf5b53f27de9738d63e22348ade
michal.browarczyk@zdt.io <michal.browarczyk@zdt.io>
2024-03-13 10:09:39 +0100

    example variables to run

--- examples/example_of_use/main.tf
+++ examples/example_of_use/main.tf
@@ -4,11 +4,13 @@
 # name - (Required) The Droplet name.
 # region - The region where the Droplet will be created.
 # size - (Required) The unique slug that indentifies the type of Droplet. You can find a list of available slugs on DigitalOcean API documentation.
-
+provider "digitalocean" {
+  token = var.do_token
+}
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "web" {
   image  = "ubuntu-20-04-x64"
   name   = "web-1"
-  region = "nyc2"
+  region = "nyc1"
   size   = "s-1vcpu-1gb"
 }
